1cbf7a069c300558754b67f5a07ce2b9546a3086
Isaac Marovitz <isaacryu@icloud.com>
2018-09-28 18:16:38 +0100

    hahahahahahahahahahahahah... fraps

---
+++ Retro-Runner/Assets/Scripts/FramesPerSecond.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class FramesPerSecond : MonoBehaviour {
+
+    public TextMeshProUGUI fpsText;
+    public float deltaTime = 0.0f;
+
+    void Update() {
+        deltaTime += (Time.unscaledDeltaTime - deltaTime) * 0.1f;
+        float fps = 1.0f / deltaTime;
+        fpsText.text = fps.ToString();
+    }
+
+    void OnGUI() {
+        float fps = 1.0f / deltaTime;
+    }
+}
