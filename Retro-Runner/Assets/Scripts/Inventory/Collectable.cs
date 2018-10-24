12898cbb92544608a9da37d6d8630fc5f6b8476e
Isaac Marovitz <isaacryu@icloud.com>
2018-10-24 09:58:47 +0100

    HDRP & Inventory

---
+++ Retro-Runner/Assets/Scripts/Inventory/Collectable.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Collectable : MonoBehaviour {
+
+    public List<string> inventoryItems;
+    public Collider[] colliders;
+    public float radius;
+    public TMP_Text collactableText;
+
+    void Start() {
+
+    }
+
+    void Update () {
+        colliders = Physics.OverlapSphere(transform.position, radius);
+        for (int i = 0; i < colliders.Length; i++) {
+            if (colliders[i].tag == "Collectable") {
+                collactableText.enabled = true;
+                if (Input.GetKeyDown(KeyCode.E)) {
+                    inventoryItems.Add(colliders[i].gameObject.name);
+                    Destroy(colliders[i].gameObject);
+                }
+            } else {
+                collactableText.enabled = false;
+            }
+        }
+    }
+
+    void OnDrawGizmosSelected () {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+
+}
