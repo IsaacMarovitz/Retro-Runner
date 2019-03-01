9ca2eae0838cbd47ec9fe3a2a65a3142e8e856da
Isaac Marovitz <isaacryu@icloud.com>
2019-03-01 11:00:15 +0000

    Added Music
    Music is now added and working correctly. Music loops and continues through scenes. The music itself is from the Retro-Aestetics pack on the Asset Store, I highly recommend it!

---
+++ Retro Runner/Retro Runner/Assets/Scripts/DontDestroyScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroyScript : MonoBehaviour {
+
+    public static DontDestroyScript instance;
+    public AudioSource music;
+
+    public void Start() {
+        if (instance != null) {
+            Destroy(this.gameObject);
+            return;
+        }
+        instance = this;
+        GameObject.DontDestroyOnLoad(this.gameObject);
+        if (music.isPlaying) 
+            return;
+        music.Play();
+    }
+}
